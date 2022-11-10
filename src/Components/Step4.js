4aec0f859abca60f91dcad8e539dec0dc7726f1f
GabrijelaJuricic <gabjuricic@gmail.com>
2022-11-10 15:19:08 +0100

    Add grid system

--- src/Components/Step4.js
+++ src/Components/Step4.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { Row, Col, Button, Container } from "react-bootstrap";
 
 const Step4 = () => {
-  return <div>Step4</div>;
+  let message = (
+    <p>
+      Molimo vas da još jednom pregledate i potvrdite unesene podatke. Ukoliko
+      želite promijeniti neki od podataka, možete to učiniti odabirom gumba za
+      uređivanje pored svake od kategorija.
+    </p>
+  );
+
+  return (
+    <>
+      <div>{message}</div>
+      <Container>
+        <Row className="mt-4">
+          <Col md={4}>MODEL VOZILA</Col>
+          <Col md={2}>
+            <Button variant="secondary">Uredi</Button>
+          </Col>
+          <Col md={4} className="p-0 md-auto">
+            IZABRANE USLUGE
+          </Col>
+          <Col md={2}>
+            <Button variant="secondary">Uredi</Button>
+          </Col>
+          <Row>
+            <Col md={6}>Kia</Col>
+            <Col md={6}>tralala</Col>
+          </Row>
+        </Row>
+        <Row className="mt-4">
+          <Col md={4}>KONTAKT PODACI</Col>
+          <Col md={2}>
+            <Button variant="secondary">Uredi</Button>
+          </Col>
+          <Row>
+            <Col md={6}>Ime i prezime:</Col>
+            <Col md={6}>Napomena:</Col>
+          </Row>
+          <Row>
+            <Col md={3}>E-mail:</Col>
+          </Row>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Step4;
